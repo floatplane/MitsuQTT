1156cf2da7250791f92dfbe2bb0879d46ac6866d
Brian Sharon <brian@floatplane.us>
2024-02-05 10:55:03 -0800

    Fix comment

--- set_output_filename.py
+++ set_output_filename.py
@@ -3,7 +3,7 @@ import subprocess
 Import("env")
 
 if not env['PIOENV'].startswith("test"):
-    # TODO(floatplane) this is duplicated in post_extra_script.py
+    # TODO(floatplane) this is duplicated in platformio.ini
     build_date = subprocess.check_output(["date", "-u", r"+%Y.%m.%d"]).decode("utf-8").strip()
     git_commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"]).decode("utf-8").strip()
 
