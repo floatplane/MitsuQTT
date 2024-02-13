c66a10a451339634c9c576af39077c40d5e45983
Brian Sharon <brian@floatplane.us>
2024-02-13 09:45:01 -0800

    Update temperature header and tests

--- lib/temperature/include/temperature.hpp
+++ lib/temperature/include/temperature.hpp
@@ -6,45 +6,66 @@
 
 class Temperature {
  private:
+  // Temperature always stored internally as Celsius
   float celsius;
 
  public:
   enum class Unit {
-    Celsius,
-    Fahrenheit
+    C,
+    F
   };
 
   // Constructor
-  explicit Temperature(float v, Unit unit)
-      : celsius(unit == Unit::Celsius ? v : fahrenheitToCelsius(v)) {
+  explicit Temperature(float value, Unit unit)
+      : celsius(unit == Unit::C ? value : fahrenheitToCelsius(value)) {
   }
 
   Temperature(const Temperature &other) : celsius(other.celsius) {
   }
+
   Temperature &operator=(const Temperature &other) {
-    celsius = other.celsius;
+    this->celsius = other.celsius;
     return *this;
   }
 
-  // Getters
   float get(const Unit unit, const float tempStep = 0.0f) const {
-    const auto value = unit == Unit::Celsius ? celsius : celsiusToFahrenheit(celsius);
+    const auto value = unit == Unit::C ? this->celsius : celsiusToFahrenheit(this->celsius);
     if (tempStep > 0.0f) {
       return std::round(value / tempStep) * tempStep;
     }
     return value;
   }
 
-  std::string toString(const Unit unit, const float tempStep = 0.0f) const {
+  // Convenience methods
+  float getCelsius(const float tempStep = 0.0f) const {
+    return get(Unit::C, tempStep);
+  }
+
+  float getFahrenheit(const float tempStep = 0.0f) const {
+    return get(Unit::F, tempStep);
+  }
+
+  std::string toString(const Unit unit, const float tempStep = 1.0f) const {
     // Would like to use std::to_chars here, but it's not available in the Arduino environment
     std::array<char, 10> str;
-    snprintf(str.data(), str.size(), "%.1f", get(unit, tempStep));
+    const auto value = get(unit, tempStep);
+    const auto digits =
+        tempStep > 0.f ? static_cast<int>(std::ceil(std::log10(1.0f / tempStep))) : 6;
+    snprintf(str.data(), str.size(), "%.*f", std::max(0, digits), value);
     return std::string(str.data());
   }
 
-  // Setters
   void set(float value, const Unit unit) {
-    celsius = unit == Unit::Celsius ? value : fahrenheitToCelsius(value);
+    this->celsius = unit == Unit::C ? value : fahrenheitToCelsius(value);
+  }
+
+  Temperature clamp(const Temperature &min, const Temperature &max) const {
+    return Temperature(std::max(std::min(this->celsius, max.celsius), min.celsius), Unit::C);
+  }
+
+  Temperature &clamp(const Temperature &min, const Temperature &max) {
+    celsius = std::max(std::min(this->celsius, max.celsius), min.celsius);
+    return *this;
   }
 
   static float celsiusToFahrenheit(const float celsius) {
--- test/native/test_temperature/temperature.cpp
+++ test/native/test_temperature/temperature.cpp
@@ -16,41 +16,47 @@ TEST_CASE("testing fahrenheit to celsius") {
 }
 
 TEST_CASE("test construction of temperature") {
-  Temperature t1(37.56f, Temperature::Unit::Celsius);
+  Temperature t1(37.56f, Temperature::Unit::C);
 
-  CHECK(std::round(t1.get(Temperature::Unit::Celsius)) == 38.f);
-  CHECK(std::round(t1.get(Temperature::Unit::Fahrenheit)) == 100.f);
+  CHECK(std::round(t1.get(Temperature::Unit::C)) == 38.f);
+  CHECK(std::round(t1.getCelsius()) == 38.f);  // shorthand for get(Unit::C)
+  CHECK(std::round(t1.get(Temperature::Unit::F)) == 100.f);
+  CHECK(std::round(t1.getFahrenheit()) == 100.f);  // shorthand for get(Unit::F)
 
-  Temperature t2(37.56f, Temperature::Unit::Fahrenheit);
+  Temperature t2(37.56f, Temperature::Unit::F);
 
-  CHECK(std::round(t2.get(Temperature::Unit::Fahrenheit)) == 38.f);
-  CHECK(std::round(t2.get(Temperature::Unit::Celsius)) == 3.f);
+  CHECK(std::round(t2.getFahrenheit()) == 38.f);
+  CHECK(std::round(t2.getCelsius()) == 3.f);
 }
 
 TEST_CASE("test rendering celsius with temperature step") {
-  Temperature t(37.56f, Temperature::Unit::Celsius);
+  Temperature t(37.56f, Temperature::Unit::C);
 
   // Can't do exact comparisons with floating point numbers - 37.6f ends up being 37.600002 or
   // something like that. So, round to 1 decimal place and compare.
-  CHECK(std::round(t.get(Temperature::Unit::Celsius, 1.0f) * 10.0f) == 380.f);
-  CHECK(std::round(t.get(Temperature::Unit::Celsius, 0.5f) * 10.0f) == 375.f);
-  CHECK(std::round(t.get(Temperature::Unit::Celsius, 0.1f) * 10.0f) == 376.f);
+  CHECK(std::round(t.getCelsius(1.0f) * 10.0f) == 380.f);
+  CHECK(std::round(t.getCelsius(0.5f) * 10.0f) == 375.f);
+  CHECK(std::round(t.getCelsius(0.1f) * 10.0f) == 376.f);
 
-  CHECK(std::round(t.get(Temperature::Unit::Fahrenheit, 1.0f) * 10.0f) == 1000.f);
-  CHECK(std::round(t.get(Temperature::Unit::Fahrenheit, 0.5f) * 10.0f) == 995.f);
-  CHECK(std::round(t.get(Temperature::Unit::Fahrenheit, 0.1f) * 10.0f) == 996.f);
+  CHECK(std::round(t.getFahrenheit(1.0f) * 10.0f) == 1000.f);
+  CHECK(std::round(t.getFahrenheit(0.5f) * 10.0f) == 995.f);
+  CHECK(std::round(t.getFahrenheit(0.1f) * 10.0f) == 996.f);
 }
 
 TEST_CASE("test toString with temperature step") {
-  Temperature t(37.56f, Temperature::Unit::Celsius);
+  Temperature t(37.560001f, Temperature::Unit::C);
 
-  CHECK(t.toString(Temperature::Unit::Celsius, 1.0f) == "38.0");
-  CHECK(t.toString(Temperature::Unit::Celsius, 0.5f) == "37.5");
-  CHECK(t.toString(Temperature::Unit::Celsius, 0.1f) == "37.6");
+  CHECK(t.toString(Temperature::Unit::C, 25.0f) == "50");
+  CHECK(t.toString(Temperature::Unit::C, 5.0f) == "40");
+  CHECK(t.toString(Temperature::Unit::C, 1.0f) == "38");
+  CHECK(t.toString(Temperature::Unit::C, 0.5f) == "37.5");
+  CHECK(t.toString(Temperature::Unit::C, 0.1f) == "37.6");
+  CHECK(t.toString(Temperature::Unit::C, 0.0f) == "37.560001");
+  CHECK(t.toString(Temperature::Unit::C, -0.1f) == "37.560001");
 
-  CHECK(t.toString(Temperature::Unit::Fahrenheit, 1.0f) == "100.0");
-  CHECK(t.toString(Temperature::Unit::Fahrenheit, 0.5f) == "99.5");
-  CHECK(t.toString(Temperature::Unit::Fahrenheit, 0.1f) == "99.6");
+  CHECK(t.toString(Temperature::Unit::F, 1.0f) == "100");
+  CHECK(t.toString(Temperature::Unit::F, 0.5f) == "99.5");
+  CHECK(t.toString(Temperature::Unit::F, 0.1f) == "99.6");
 }
 
 int main(int argc, char **argv) {
